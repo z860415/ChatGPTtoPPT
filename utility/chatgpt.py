4e15cefa7d6d690945fa59daefe394c4f4503a82
Eric <z860415@gmail.com>
2023-04-06 02:12:31 +0800

    初始化專案

---
+++ utility/chatgpt.py
@@ -0,0 +1,32 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_notes(title:str, name: str):
+    request_string = f'''
+    請幫我把「{title}」主題課程，以好的方面闡述，整理成4000字三級結構簡報，以 Markdown 標記語法原始程式碼方便複製使用
+    '''
+    openai.api_key = os.getenv('GPT_TOKEN')
+
+    messages = []
+    msg = request_string
+    messages.append({"role":"user","content":msg})   # 添加 user 回應
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=2000,
+        temperature=0.5,
+        messages=messages
+    )
+    # ai_msg = response.choices[0].message.content.replace('\n','')
+    ai_msg = response.choices[0].message.content
+    messages.append({"role":"assistant","content":ai_msg})   # 添加 ChatGPT 回應
+    # print(f'ai > {ai_msg}')
+    file = open('../output.txt', 'w', encoding='utf-8')
+    file.write(ai_msg)
+    file.close()
+
+
+if __name__ == "__main__":
+    generate_notes(title='毛語錄', name="李宗育")
